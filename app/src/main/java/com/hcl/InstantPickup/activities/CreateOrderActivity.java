4d4d55b3d1d69fdb10ef035ecd00c51e42a00c2b
alicelambda <alicereuterdev@gmail.com>
2019-07-30 11:46:32 -0700

    escaped bobs bagel string

--- app/src/main/java/com/hcl/InstantPickup/activities/CreateOrderActivity.java
+++ app/src/main/java/com/hcl/InstantPickup/activities/CreateOrderActivity.java
@@ -29,7 +29,7 @@ public class CreateOrderActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_order);
+      //  setContentView(R.layout.activity_create_order);
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://10.0.2.2:8080/")
                 .addConverterFactory(GsonConverterFactory.create())

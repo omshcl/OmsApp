8958a3f62adf6cfd4324d62eefb1e40c69fde869
Shivaprakash Sivagurunathan <s.shivaprakash29@gmail.com>
2019-08-02 11:58:20 -0500

    on my way button ready pickup changes

--- app/src/main/java/com/hcl/InstantPickup/activities/HomeFragment.java
+++ app/src/main/java/com/hcl/InstantPickup/activities/HomeFragment.java
@@ -1,17 +1,21 @@
 package com.hcl.InstantPickup.activities;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -36,7 +40,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        tl=(TableLayout)v.findViewById(R.id.table_layout);
+        tl = (TableLayout) v.findViewById(R.id.table_layout);
 
         return v;
     }
@@ -44,53 +48,26 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        final String username=SingletonClass.getInstance().getName();
-        TextView textView=(TextView)view.findViewById(R.id.textHomeWelcome);
+        final String username = SingletonClass.getInstance().getName();
+        TextView textView = (TextView) view.findViewById(R.id.textHomeWelcome);
 
-        textView.setText("Welcome"+" "+username);
+        textView.setText("Welcome" + " " + username);
 
-        final Bundle bundle=this.getArguments();
-        if(bundle==null) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getString(R.string.backend_url))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        apiCalls = retrofit.create(ApiCalls.class);
 
+        getOrders(username);
 
-
-            view.findViewById(R.id.button2).setVisibility(View.GONE);
-
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(getString(R.string.backend_url))
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-            apiCalls = retrofit.create(ApiCalls.class);
-
-            getOrders(username);
-        }
-
-        if(bundle!=null) {
-            view.setVisibility(View.VISIBLE);
-
-
-            view.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view2) {
-                    view2.setVisibility(View.VISIBLE);
-
-                }
-            });
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(getString(R.string.backend_url))
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-            apiCalls = retrofit.create(ApiCalls.class);
-
-            getOrders(username);
-        }
 
         //WRITE ACTIVITY CODE HERE
         // ex. view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() ....
 
     }
 
-    private void getOrders(String name){
+    private void getOrders(String name) {
 
         final JsonObject username = new JsonObject();
         username.addProperty("username", name);
@@ -108,10 +85,10 @@ public class HomeFragment extends Fragment {
                     System.out.println("Code: " + response.code());
                     return;
                 }
-
+                int x = 20;
                 // Request is successful
                 JsonArray orders = response.body();
-                TableRow tr_head=new TableRow(getActivity());
+                TableRow tr_head = new TableRow(getActivity());
                 tr_head.setId(0);
                 tr_head.setBackgroundColor(Color.BLUE);
                 tr_head.setLayoutParams(new TableLayout.LayoutParams(
@@ -121,23 +98,30 @@ public class HomeFragment extends Fragment {
                 label_hello.setId(0);
                 label_hello.setText("ORDER ID");
                 label_hello.setTextColor(Color.WHITE);          // part2
-                label_hello.setPadding(70, 70, 70, 70);
+                label_hello.setPadding(x, x, x, x);
                 tr_head.addView(label_hello);// add the column to the table row here
-                int j=1;
+                int j = 1;
                 TextView label_android = new TextView(getActivity());    // part3
                 label_android.setId(j);// define id that must be unique
                 label_android.setText("DEMAND TYPE"); // set the text for the header
                 label_android.setTextColor(Color.WHITE); // set the color
-                label_android.setPadding(70, 70, 70, 70); // set the padding (if required)
+                label_android.setPadding(x, x, x, x); // set the padding (if required)
                 tr_head.addView(label_android);
 
 
                 TextView label_d = new TextView(getActivity());    // part3
-                label_d.setId(j+1);// define id that must be unique
+                label_d.setId(j + 1);// define id that must be unique
                 label_d.setText("TOTAL"); // set the text for the header
                 label_d.setTextColor(Color.WHITE); // set the color
-                label_d.setPadding(70, 70, 70, 70); // set the padding (if required)
+                label_d.setPadding(x, x, x, x); // set the padding (if required)
                 tr_head.addView(label_d);
+
+                TextView label_d1 = new TextView(getActivity());    // part3
+                label_d1.setId(j + 1);// define id that must be unique
+                label_d1.setText("ACTION"); // set the text for the header
+                label_d1.setTextColor(Color.WHITE); // set the color
+                label_d1.setPadding(x, x, x, x); // set the padding (if required)
+                tr_head.addView(label_d1);
 
                 tl.addView(tr_head, new TableLayout.LayoutParams(
                         TableLayout.LayoutParams.MATCH_PARENT,                    //part4
@@ -147,15 +131,16 @@ public class HomeFragment extends Fragment {
                 TextView[] textArray = new TextView[orders.size()];
                 TextView[] textArray1 = new TextView[orders.size()];
                 TextView[] textArray2 = new TextView[orders.size()];
+
                 TableRow[] tr_head1 = new TableRow[orders.size()];
 
-                for(int i=0;i<orders.size();i++) {
+                for (int i = 0; i < orders.size(); i++) {
                     JsonObject order = orders.get(i).getAsJsonObject();
-                    String demand_type = order.get("demand_type").toString();
+                    String demand_type = order.get("demand_type").getAsString();
                     String total = order.get("total").toString();
                     String order_id = order.get("id").toString();
                     tr_head1[i] = new TableRow(getActivity());
-                    tr_head1[i].setId(i+1);
+                    tr_head1[i].setId(i + 1);
                     tr_head1[i].setBackgroundColor(Color.GRAY);
                     tr_head1[i].setLayoutParams(new TableLayout.LayoutParams(
                             TableLayout.LayoutParams.MATCH_PARENT,
@@ -164,34 +149,52 @@ public class HomeFragment extends Fragment {
                     // Here create the TextView dynamically
 
                     textArray[i] = new TextView(getActivity());
-                    textArray[i].setId(i+111);
+                    textArray[i].setId(i + 111);
                     textArray[i].setText(order_id);
                     textArray[i].setTextColor(Color.WHITE);
-                    textArray[i].setPadding(70, 70, 70, 70);
+                    textArray[i].setPadding(x, x, x, x);
                     tr_head1[i].addView(textArray[i]);
 
 
                     textArray1[i] = new TextView(getActivity());
-                    textArray1[i].setId(i+111);
+                    textArray1[i].setId(i + 111);
                     textArray1[i].setText(demand_type);
                     textArray1[i].setTextColor(Color.WHITE);
-                    textArray1[i].setPadding(70, 70, 70, 70);
+                    textArray1[i].setPadding(x, x, x, x);
                     tr_head1[i].addView(textArray1[i]);
 
 
                     textArray2[i] = new TextView(getActivity());
-                    textArray2[i].setId(i+111);
+                    textArray2[i].setId(i + 111);
                     textArray2[i].setText(total);
                     textArray2[i].setTextColor(Color.WHITE);
-                    textArray2[i].setPadding(70, 70, 70, 70);
+                    textArray2[i].setPadding(x, x, x, x);
                     tr_head1[i].addView(textArray2[i]);
+
+                    System.out.println(demand_type);
+                    if (demand_type.equals("READY_PICKUP")) {
+                        System.out.println("Inside demandtype");
+                        Button btn = new Button(getActivity());
+                        btn.setText("ON MY WAY");
+                        btn.setId(j + 4);
+                        btn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                YourStoreFragment yourStoreFragment = new YourStoreFragment();
+                                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                                fragmentTransaction.replace(((ViewGroup)getView().getParent()).getId(), yourStoreFragment);
+                                fragmentTransaction.addToBackStack(null);
+                                fragmentTransaction.commit();
+
+                            }
+                        });
+                        tr_head1[i].addView(btn);
+                    }
+
 
                     tl.addView(tr_head1[i], new TableLayout.LayoutParams(
                             TableLayout.LayoutParams.MATCH_PARENT,
                             TableLayout.LayoutParams.WRAP_CONTENT));
-
-
-
 
 
                 }
